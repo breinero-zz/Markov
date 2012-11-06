f66b2699e42ee217ca064ab8a2e3e61b41e6379c
Bryan Reinero <bryan.reinero@10gen.com>
2012-11-06 14:38:47 -0500

    Removing files from misnamed package

--- src/com/bryanreinero/merkle/Callback.java
+++
@@ -1,5 +0,0 @@
-package com.bryanreinero.merkle;
-
-public interface Callback <T extends Visitor<T>> {
-	public void accept ( T  responder ); 
-}
--- src/com/bryanreinero/merkle/Visitor.java
+++
@@ -1,6 +0,0 @@
-package com.bryanreinero.merkle;
-
-public interface Visitor<T extends Visitor<T>> {
-	public void visit(Vertex<T> vertex);
-	public double getRandomDouble();
-}
